72e04ecae66ceba4432bf1da157b50d359f4460f
Aygen5 <aygenyildirim27@gmail.com>
2026-06-12 16:21:00 +0300

    bugunun calismalari

---
+++ 12 haziran cuma/ilk-ders/src/component/Demo8ReadOnlyProps.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const DegistirilmeyenKart=(props)=>{
+    const deneVeHataGoster=()=>{
+        try{
+            props.baslik="Yeni Başlık";
+
+        }
+        catch(hata){
+            alert("Hata Yakalandı! Props Değiştirilmez"+hata.message);
+
+        }
+    }
+
+    return (
+        <div className="card">
+            <h4 className="font-bold">{props.baslik}</h4>
+            <p className="text-gray-500">Gelen Prop Değeri{props.baslik}</p>
+            <button onClick={deneVeHataGoster} className="btn-red">Prop Değiştirmeyi Dene</button>
+        </div>
+    )
+}
+
+
+
+const Demo8ReadonlyProps=()=>{
+    return(
+        <div className="p-4">
+            <h3 className="text-xl font-bold">Demo 8: Salt Okunur(readonly)Props</h3>
+            <div className="mt-4">
+                <DegistirilmeyenKart baslik="Değiştirilemeyen Kart Başlık" />
+            </div>
+        </div>
+    );
+};
+
+export default Demo8ReadonlyProps;
